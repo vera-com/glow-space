5e63590d2e91077100886b566504fd0e99c97c24
vera-com <withneyugomma@gmail.com>
2026-06-01 23:24:24 +0200

    Create and style bookings page.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def index(request):
+def home(request):
     return render(request, 'home/index.html')
+
+
+def bookings(request):
+    return render(request, 'home/bookings.html')
