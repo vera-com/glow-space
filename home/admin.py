55f3b233e3abc02d7794b9148f13b3db654d6eb7
vera-com <withneyugomma@gmail.com>
2026-06-06 20:05:39 +0200

    Customize booking admin and improve booking management.

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
 from .models import Booking
 
-admin.site.register(Booking)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'service',
+        'preferred_date',
+        'preferred_time',
+        'created_on',
+    )
+
+    list_filter = (
+        'service',
+        'preferred_date',
+        'created_on',
+    )
+
+    search_fields = (
+        'name',
+        'email',
+        'service',
+    )
+
+    ordering = ('-created_on',)
